0e4fa38c334948a117d7b77f9bdca1f88879267d
agsjorda <agsjorda@gmail.com>
2023-12-02 02:50:52 +0800

    Initial Files and StateMachine

---
+++ Assets/PlayerState.cs
@@ -0,0 +1,34 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerState
+{
+    protected PlayerStateMachine stateMachine;
+    protected Player player;
+    private string animBoolName;
+
+    public PlayerState(Player _player, PlayerStateMachine _stateMachine, String _animBoolName)
+    {
+        this.player = _player;
+        this.stateMachine = _stateMachine;
+        this.animBoolName = _animBoolName;
+    }
+
+    public virtual void Enter()
+    {
+        Debug.Log("I enter " + animBoolName);
+    }
+
+    public virtual void Update()
+    {
+        Debug.Log("I am in " + animBoolName);
+    }
+
+    public virtual void Exit()
+    {
+        Debug.Log("I exit " + animBoolName);
+    }
+
+}
