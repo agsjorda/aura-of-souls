743e9c18719f5b9a82b3d1989462703fb568430e
agsjorda <agsjorda@gmail.com>
2023-12-04 00:15:44 +0800

    added wall slide and jump

--- Assets/PlayerAirState.cs
+++ Assets/PlayerAirState.cs
@@ -21,8 +21,13 @@ public class PlayerAirState : PlayerState
     public override void Update()
     {
         base.Update();
+        if(player.isWallDetected()) 
+            stateMachine.ChangeState(player.wallSlideState);
 
         if(player.isGroundDetected())
             stateMachine.ChangeState(player.idleState);
+
+        if (xInput != 0)
+            player.SetVelocity(player.moveSpeed * .8f * xInput, rb.velocity.y);
     }
 }
